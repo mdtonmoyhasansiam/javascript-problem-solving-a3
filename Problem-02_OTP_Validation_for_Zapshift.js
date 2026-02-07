7f673745d4de5276fbe7e3c00f4fb3476d90a6b1
mdtonmoyhasansiam <tonmoy.hasan13534@gmail.com>
2026-02-07 13:49:43 +0600

    This problem solved

---
+++ Problem-02_OTP_Validation_for_Zapshift.js
@@ -0,0 +1,17 @@
+
+function validOtp(otp) {
+    if(
+        typeof otp !== "string"
+    ){
+        return "Invalid"
+    }
+
+    if(
+        otp.length === 8 && otp.startsWith("ph-")
+    ){
+        return true
+    }
+    else{
+        return false
+    }
+}
